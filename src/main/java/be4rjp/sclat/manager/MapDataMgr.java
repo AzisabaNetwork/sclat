eeb540d0ce7d68c7027080ed1991c204871df3dd
sysnote8main <sysnote8main@gmail.com>
2025-05-14 22:48:38 +0900

    feat: add util and use it in MapDataMgr

--- src/main/java/be4rjp/sclat/manager/MapDataMgr.java
+++ src/main/java/be4rjp/sclat/manager/MapDataMgr.java
@@ -9,6 +9,7 @@ import be4rjp.sclat.data.Path;
 import be4rjp.sclat.data.WiremeshListTask;
 import static org.bukkit.Bukkit.getServer;
 
+import be4rjp.sclat.utils.ConfigUtil;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.configuration.ConfigurationSection;
@@ -29,68 +30,32 @@ public class MapDataMgr {
             MapData map = new MapData(mapName);
             String WorldName = conf.getMapConfig().getString("Maps." + mapName + ".WorldName");
             assert WorldName != null;
+
             World world = getServer().getWorld(WorldName);
-            
-            int ix = conf.getMapConfig().getInt("Maps." + mapName + ".Intro.X");
-            int iy = conf.getMapConfig().getInt("Maps." + mapName + ".Intro.Y");
-            int iz = conf.getMapConfig().getInt("Maps." + mapName + ".Intro.Z");
-            int iyaw = conf.getMapConfig().getInt("Maps." + mapName + ".Intro.Yaw");
-            Location il = new Location(world, ix, iy, iz);
-            il.setYaw(iyaw);
+
+            Location introLocation = ConfigUtil.getLocationWithYaw(mapsSection, mapName + ".Intro", world);
             
             double intromovex = conf.getMapConfig().getDouble("Maps." + mapName + ".Intro.MoveX");
             double intromovey = conf.getMapConfig().getDouble("Maps." + mapName + ".Intro.MoveY");
             double intromovez = conf.getMapConfig().getDouble("Maps." + mapName + ".Intro.MoveZ");
-            
-            int t0x = conf.getMapConfig().getInt("Maps." + mapName + ".Team0.X");
-            int t0y = conf.getMapConfig().getInt("Maps." + mapName + ".Team0.Y");
-            int t0z = conf.getMapConfig().getInt("Maps." + mapName + ".Team0.Z");
-            int t0yaw = conf.getMapConfig().getInt("Maps." + mapName + ".Team0.Yaw");
-            Location t0l = new Location(world, t0x, t0y, t0z);
-            t0l.setX(t0l.getX() + 0.5);
-            t0l.setZ(t0l.getZ() + 0.5);
-            t0l.setYaw(t0yaw);
-            
-            int t1x = conf.getMapConfig().getInt("Maps." + mapName + ".Team1.X");
-            int t1y = conf.getMapConfig().getInt("Maps." + mapName + ".Team1.Y");
-            int t1z = conf.getMapConfig().getInt("Maps." + mapName + ".Team1.Z");
-            int t1yaw = conf.getMapConfig().getInt("Maps." + mapName + ".Team1.Yaw");
-            Location t1l = new Location(world, t1x, t1y, t1z);
-            t1l.setX(t1l.getX() + 0.5);
-            t1l.setZ(t1l.getZ() + 0.5);
-            t1l.setYaw(t1yaw);
-            
-            int t0intx = conf.getMapConfig().getInt("Maps." + mapName + ".Team0IntroLoc.X");
-            int t0inty = conf.getMapConfig().getInt("Maps." + mapName + ".Team0IntroLoc.Y");
-            int t0intz = conf.getMapConfig().getInt("Maps." + mapName + ".Team0IntroLoc.Z");
-            int t0intyaw = conf.getMapConfig().getInt("Maps." + mapName + ".Team0IntroLoc.Yaw");
-            Location t0intl = new Location(world, t0intx, t0inty, t0intz);
-            t0intl.setYaw(t0intyaw);
-            
-            int t1intx = conf.getMapConfig().getInt("Maps." + mapName + ".Team1IntroLoc.X");
-            int t1inty = conf.getMapConfig().getInt("Maps." + mapName + ".Team1IntroLoc.Y");
-            int t1intz = conf.getMapConfig().getInt("Maps." + mapName + ".Team1IntroLoc.Z");
-            int t1intyaw = conf.getMapConfig().getInt("Maps." + mapName + ".Team1IntroLoc.Yaw");
-            Location t1intl = new Location(world, t1intx, t1inty, t1intz);
-            t1intl.setYaw(t1intyaw);
-            
-            int rlocx = conf.getMapConfig().getInt("Maps." + mapName + ".ResultLoc.X");
-            int rlocy = conf.getMapConfig().getInt("Maps." + mapName + ".ResultLoc.Y");
-            int rlocz = conf.getMapConfig().getInt("Maps." + mapName + ".ResultLoc.Z");
-            int rlocyaw = conf.getMapConfig().getInt("Maps." + mapName + ".ResultLoc.Yaw");
-            Location rloc = new Location(world, rlocx, rlocy, rlocz);
-            rloc.setYaw(rlocyaw);
-            rloc.setPitch(90);
-            
-            int tlocx = conf.getMapConfig().getInt("Maps." + mapName + ".WaitLoc.X");
-            int tlocy = conf.getMapConfig().getInt("Maps." + mapName + ".WaitLoc.Y");
-            int tlocz = conf.getMapConfig().getInt("Maps." + mapName + ".WaitLoc.Z");
-            Location tloc = new Location(world, tlocx, tlocy, tlocz);
-            
-            int nlocx = conf.getMapConfig().getInt("Maps." + mapName + ".NoBlockLoc.X");
-            int nlocy = conf.getMapConfig().getInt("Maps." + mapName + ".NoBlockLoc.Y");
-            int nlocz = conf.getMapConfig().getInt("Maps." + mapName + ".NoBlockLoc.Z");
-            Location nloc = new Location(world, nlocx, nlocy, nlocz);
+
+            Location team0Location = ConfigUtil.getLocationWithYaw(mapsSection, mapName + ".Team0", world);
+            team0Location.setX(team0Location.getX() + 0.5);
+            team0Location.setZ(team0Location.getZ() + 0.5);
+
+            Location team1Location = ConfigUtil.getLocationWithYaw(mapsSection, mapName + ".Team1", world);
+            team1Location.setX(team1Location.getX() + 0.5);
+            team1Location.setZ(team1Location.getZ() + 0.5);
+
+            Location team0IntroLocation = ConfigUtil.getLocationWithYaw(mapsSection, mapName + ".Team0IntroLoc", world);
+            Location team1IntroLocation = ConfigUtil.getLocationWithYaw(mapsSection, mapName + ".Team1IntroLoc", world);
+
+            Location resultLocation = ConfigUtil.getLocationWithYaw(mapsSection, mapName + ".ResultLoc", world);
+            resultLocation.setPitch(90);
+
+            Location waitLocation = ConfigUtil.getLocation(mapsSection, mapName + ".WaitLoc", world);
+
+            Location noBlockLocation = ConfigUtil.getLocation(mapsSection, mapName + ".NoBlockLoc", world);
             
             if(conf.getMapConfig().contains("Maps." + mapName + ".Path")){
                 for (String pathname : conf.getMapConfig().getConfigurationSection("Maps." + mapName + ".Path").getKeys(false)){
@@ -160,14 +125,14 @@ public class MapDataMgr {
             }
             
             
-            map.setIntro(il);
-            map.setTeam0Loc(t0l);
-            map.setTeam1Loc(t1l);
-            map.setTeam0Intro(t0intl);
-            map.setTeam1Intro(t1intl);
-            map.setResultLoc(rloc);
-            map.setTaikibasyo(tloc);
-            map.setNoBlockLocation(nloc);
+            map.setIntro(introLocation);
+            map.setTeam0Loc(team0Location);
+            map.setTeam1Loc(team1Location);
+            map.setTeam0Intro(team0IntroLocation);
+            map.setTeam1Intro(team1IntroLocation);
+            map.setResultLoc(resultLocation);
+            map.setTaikibasyo(waitLocation);
+            map.setNoBlockLocation(noBlockLocation);
             
             map.setIntroMoveX(intromovex);
             map.setIntroMoveY(intromovey);
--- src/main/java/be4rjp/sclat/utils/ConfigUtil.java
+++ src/main/java/be4rjp/sclat/utils/ConfigUtil.java
@@ -6,12 +6,16 @@ import org.bukkit.configuration.ConfigurationSection;
 
 public class ConfigUtil {
     public static Location getLocation(ConfigurationSection section, String path, World world) {
-        Location loc = new Location(
+        return new Location(
                 world,
                 section.getInt(path + ".X"),
                 section.getInt(path + ".Y"),
                 section.getInt(path + ".Z")
         );
+    }
+
+    public static Location getLocationWithYaw(ConfigurationSection section, String path, World world) {
+        Location loc = getLocation(section, path, world);
         loc.setYaw(section.getInt(path + ".Yaw"));
         return loc;
     }
