e11923e5723d390142920c3dcfc9289512ef24a6
sysnote8main <sysnote8main@gmail.com>
2025-05-14 23:22:29 +0900

    fix: use eachMapSection in SetupMap method

--- src/main/java/be4rjp/sclat/manager/MapDataMgr.java
+++ src/main/java/be4rjp/sclat/manager/MapDataMgr.java
@@ -27,38 +27,41 @@ public class MapDataMgr {
         assert mapsSection != null;
 
         for (String mapName : mapsSection.getKeys(false)){
+            ConfigurationSection eachMapSection = mapsSection.getConfigurationSection(mapName);
+            assert eachMapSection != null;
+
             MapData map = new MapData(mapName);
-            String WorldName = conf.getMapConfig().getString("Maps." + mapName + ".WorldName");
+            String WorldName = eachMapSection.getString("WorldName");
             assert WorldName != null;
 
             World world = getServer().getWorld(WorldName);
 
-            Location introLocation = ConfigUtil.getLocationWithYaw(mapsSection, world, mapName + ".Intro");
-            
-            double intromovex = conf.getMapConfig().getDouble("Maps." + mapName + ".Intro.MoveX");
-            double intromovey = conf.getMapConfig().getDouble("Maps." + mapName + ".Intro.MoveY");
-            double intromovez = conf.getMapConfig().getDouble("Maps." + mapName + ".Intro.MoveZ");
+            Location introLocation = ConfigUtil.getLocationWithYaw(eachMapSection, world, "Intro");
 
-            Location team0Location = ConfigUtil.getLocationWithYaw(mapsSection, world, mapName + ".Team0");
+            double intromovex = eachMapSection.getDouble("Intro.MoveX");
+            double intromovey = eachMapSection.getDouble("Intro.MoveY");
+            double intromovez = eachMapSection.getDouble("Intro.MoveZ");
+
+            Location team0Location = ConfigUtil.getLocationWithYaw(eachMapSection, world, "Team0");
             team0Location.setX(team0Location.getX() + 0.5);
             team0Location.setZ(team0Location.getZ() + 0.5);
 
-            Location team1Location = ConfigUtil.getLocationWithYaw(mapsSection, world, mapName + ".Team1");
+            Location team1Location = ConfigUtil.getLocationWithYaw(eachMapSection, world, "Team1");
             team1Location.setX(team1Location.getX() + 0.5);
             team1Location.setZ(team1Location.getZ() + 0.5);
 
-            Location team0IntroLocation = ConfigUtil.getLocationWithYaw(mapsSection, world, mapName + ".Team0IntroLoc");
-            Location team1IntroLocation = ConfigUtil.getLocationWithYaw(mapsSection, world, mapName + ".Team1IntroLoc");
+            Location team0IntroLocation = ConfigUtil.getLocationWithYaw(eachMapSection, world, "Team0IntroLoc");
+            Location team1IntroLocation = ConfigUtil.getLocationWithYaw(eachMapSection, world, "Team1IntroLoc");
 
-            Location resultLocation = ConfigUtil.getLocationWithYaw(mapsSection, world, mapName + ".ResultLoc");
+            Location resultLocation = ConfigUtil.getLocationWithYaw(eachMapSection, world, "ResultLoc");
             resultLocation.setPitch(90);
 
-            Location waitLocation = ConfigUtil.getLocation(mapsSection, world, mapName + ".WaitLoc");
+            Location waitLocation = ConfigUtil.getLocation(eachMapSection, world, "WaitLoc");
 
-            Location noBlockLocation = ConfigUtil.getLocation(mapsSection, world, mapName + ".NoBlockLoc");
+            Location noBlockLocation = ConfigUtil.getLocation(eachMapSection, world, "NoBlockLoc");
             
-            if(conf.getMapConfig().contains("Maps." + mapName + ".Path")){
-                ConfigurationSection pathSection = conf.getMapConfig().getConfigurationSection("Maps." + mapName + ".Path");
+            if(eachMapSection.contains("Path")){
+                ConfigurationSection pathSection = eachMapSection.getConfigurationSection("Path");
                 assert pathSection != null;
                 for (String pathname : pathSection.getKeys(false)){
                     Location from = ConfigUtil.getLocation(pathSection, world, pathname + ".From");
@@ -69,8 +72,8 @@ public class MapDataMgr {
                 }
             }
             
-            if(conf.getMapConfig().contains("Maps." + mapName + ".Area")){
-                ConfigurationSection areaSection = conf.getMapConfig().getConfigurationSection("Maps." + mapName + ".Area");
+            if(eachMapSection.contains("Area")){
+                ConfigurationSection areaSection = eachMapSection.getConfigurationSection("Area");
                 assert areaSection != null;
                 for (String Areaname : areaSection.getKeys(false)){
                     map.setCanAreaBattle(true);
@@ -86,11 +89,11 @@ public class MapDataMgr {
                 }
             }
             
-            boolean canpaintbblock = conf.getMapConfig().getBoolean("Maps." + mapName + ".CanPaintBarrierBlock", false);
+            boolean canPaintBBlock = eachMapSection.getBoolean("CanPaintBarrierBlock", false);
             
             
-            if(conf.getMapConfig().contains("Maps." + mapName + ".Wiremesh")){
-                ConfigurationSection wiremeshSection = conf.getMapConfig().getConfigurationSection("Maps." + mapName + ".Wiremesh");
+            if(eachMapSection.contains("Wiremesh")){
+                ConfigurationSection wiremeshSection = eachMapSection.getConfigurationSection("Wiremesh");
                 assert wiremeshSection != null;
 
                 boolean trapDoor = wiremeshSection.getBoolean("TrapDoor", false);
@@ -108,8 +111,8 @@ public class MapDataMgr {
                 map.setWiremeshListTask(wmListTask);
             }
     
-            if(conf.getMapConfig().contains("Maps." + mapName + ".VoidY")){
-                map.setVoidY(conf.getMapConfig().getInt("Maps." + mapName + ".VoidY"));
+            if(eachMapSection.contains("VoidY")){
+                map.setVoidY(eachMapSection.getInt("VoidY"));
             }
             
             
@@ -127,17 +130,14 @@ public class MapDataMgr {
             map.setIntroMoveZ(intromovez);
             
             
-            map.setCanPaintBBlock(canpaintbblock);
-            
-            //Main.getPlugin().getServer().createWorld(new WorldCreator(WorldName));
+            map.setCanPaintBBlock(canPaintBBlock);
             
             Main.getPlugin().getLogger().info(mapName);
             
             map.setWorldName(WorldName);
             
             allMapCount++;
-            
-            //DataMgr.setMap(mapname, map);
+
             DataMgr.addMapList(map);
         }
     }
