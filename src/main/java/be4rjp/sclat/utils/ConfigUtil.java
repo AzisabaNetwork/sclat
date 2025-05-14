c1b0dd634294968e224fe41db67605144bcf3f6e
sysnote8main <sysnote8main@gmail.com>
2025-05-14 20:23:30 +0900

    feat: add getLocation in ConfigUtil

---
+++ src/main/java/be4rjp/sclat/utils/ConfigUtil.java
@@ -0,0 +1,18 @@
+package be4rjp.sclat.utils;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.configuration.ConfigurationSection;
+
+public class ConfigUtil {
+    public static Location getLocation(ConfigurationSection section, String path, World world) {
+        Location loc = new Location(
+                world,
+                section.getInt(path + ".X"),
+                section.getInt(path + ".Y"),
+                section.getInt(path + ".Z")
+        );
+        loc.setYaw(section.getInt(path + ".Yaw"));
+        return loc;
+    }
+}
