ed5b3555e6a39cef837c7fbecdfe35cdeea92877
sysnote8main <sysnote8main@gmail.com>
2025-06-15 11:33:38 +0900

    chore: use link tag instead of direct url

--- src/main/java/be4rjp/sclat/MineStat.java
+++ src/main/java/be4rjp/sclat/MineStat.java
@@ -1,14 +1,14 @@
 package be4rjp.sclat;
 
-/**
- * from https://github.com/ldilley/minestat/blob/master/Java/me/dilley/MineStat.java
- */
-
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+
+/**
+ * from <a href="https://github.com/ldilley/minestat/blob/master/Java/me/dilley/MineStat.java">Minestat.java</a>
+ */
 
 public class MineStat {
     public static final byte NUM_FIELDS = 6;     // expected number of fields returned from server after query
