3aae7963a693fd19f1d25abd638be53b68fa4352
sysnote8main <sysnote8main@gmail.com>
2026-01-24 22:09:30 +0900

    chore: fix format

--- src/main/java/be4rjp/sclat/api/SimpleRunnable.java
+++ src/main/java/be4rjp/sclat/api/SimpleRunnable.java
@@ -4,21 +4,23 @@ import be4rjp.sclat.Sclat;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 
+import java.util.function.Consumer;
+
 public class SimpleRunnable {
-	public static BukkitRunnable from(Runnable action) {
+	public static BukkitRunnable from(Consumer<Runnable> action) {
 		return new BukkitRunnable() {
 			@Override
 			public void run() {
-				action.run();
+				action.accept(this::cancel);
 			}
 		};
 	}
 
-    public static BukkitTask runTaskLater(Runnable action, long delay) {
-        return from(action).runTaskLater(Sclat.getPlugin(), delay);
-    }
+	public static BukkitTask runTaskLater(Consumer<Runnable> action, long delay) {
+		return from(action).runTaskLater(Sclat.getPlugin(), delay);
+	}
 
-    public static BukkitTask runTaskTimer(Runnable action, long delay, long period) {
-        return from(action).runTaskTimer(Sclat.getPlugin(), delay, period);
-    }
+	public static BukkitTask runTaskTimer(Consumer<Runnable> action, long delay, long period) {
+		return from(action).runTaskTimer(Sclat.getPlugin(), delay, period);
+	}
 }
