6fbe265285b61e4ec7e20e476ba566c365976823
sysnote8main <sysnote8main@gmail.com>
2026-02-22 11:09:29 +0900

    fix: forget to add serializable

--- src/main/kotlin/be4rjp/sclat/config/MapsConfig.kt
+++ src/main/kotlin/be4rjp/sclat/config/MapsConfig.kt
@@ -1,5 +1,8 @@
 package be4rjp.sclat.config
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class MapsConfig(
     val maps: Map<String, MapConfig> = mapOf("SimpleMapForTesting" to MapConfig()),
 )
